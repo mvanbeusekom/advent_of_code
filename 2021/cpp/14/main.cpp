0322d9290809d28ec3b7c7ba26f628c5cbd09f0c
Maurits van Beusekom <maurits@baseflow.com>
2021-12-16 09:20:10 +0100

    Solution for puzzle 14

---
+++ 2021/cpp/14/main.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <vector>
+#include <cmath>
+#include "../shared/utils.h"
+
+
+int main () {
+    std::vector<std::string> data = readData();
+    std::vector<int> input = split<int>(
+            data[0],
+            ",",
+            [](std::string str) { return std::stoi(str); });
+    std::sort(input.begin(), input.end());
+    int lowestConsumption = -1;
+
+    for (int i = input[0]; i <= input[input.size() - 1]; ++i) {
+        int consumption = 0;
+        for (std::vector<int>::iterator it = input.begin(); it != input.end(); ++it) {
+            int x = std::abs((*it) - i) ;
+            consumption += (x + 1) * (x / 2) + (x % 2) * (x / 2 + 1);
+        }
+
+        if (lowestConsumption == -1 || consumption < lowestConsumption) {
+            lowestConsumption = consumption;
+        }
+    }
+
+    std::cout << "Answer: " << lowestConsumption << std::endl;
+    std::cout << "Press enter to exit the application." << std::endl;
+    std::cin.get();
+}
